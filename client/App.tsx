4194634673f0d0ceadd385d1900ef61f063f4d9e
jonathan dannel <usixjad@gmail.com>
2020-02-18 08:35:27 -0500

    request camera/gallery permissions in mobile app

--- client/App.tsx
+++ client/App.tsx
@@ -1,10 +1,61 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState, useCallback } from "react";
+import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [cameraPermissions, setCameraPermissions] = useState(false);
+  const [galleryPermissions, setGalleryPermissions] = useState(false);
+  const [galleryPhoto, setGalleryPhoto] = useState({});
+
+  const getGalleryPermissions = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    if (status === "granted") {
+      setGalleryPermissions(true);
+    }
+  };
+
+  const getCameraPermissions = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    if (status === "granted") {
+      setCameraPermissions(true);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      if (!galleryPermissions) {
+        getGalleryPermissions();
+      } else if (!cameraPermissions) {
+        getCameraPermissions();
+      }
+    })();
+  }, [galleryPermissions, cameraPermissions]);
+
+  const pickPhotoFromGallery = useCallback(async () => {
+    if (!galleryPermissions) {
+      getGalleryPermissions();
+    } else {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      setGalleryPhoto(result);
+      console.log(result);
+    }
+  }, [galleryPermissions]);
+
+  const takeCameraPhoto = useCallback(() => {
+    return null;
+  }, [cameraPermissions]);
+
   return (
     <View style={styles.container}>
-      <Text>Open</Text>
+      <Button onPress={pickPhotoFromGallery} title="gallery">
+        {" "}
+        Choose from gallery{" "}
+      </Button>
+      <Button onPress={takeCameraPhoto} title="camera">
+        {" "}
+        Take a photo{" "}
+      </Button>
     </View>
   );
 }
