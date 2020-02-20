21f75e5b818331546cccfc4c26307d3b37ce3e0b
jonathan dannel <usixjad@gmail.com>
2020-02-20 09:39:28 -0500

    start decoding base64

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ def scan():
     data = request.get_json()
     base64_string = data["base64"]
 
+    print(base64_string)
+
     # image = ScanRemoteImage(image_url)
     # image_text = image.get_text()
     
--- ocr.py
+++ ocr.py
@@ -2,6 +2,15 @@ from PIL import Image
 from pytesseract import image_to_string
 from io import BytesIO
 import requests
+import base64
+
+class ScanBase64Image:
+    def __init__(self, base64string):
+        self.base64string = base64string
+    
+    def decode(self):
+        decoded = base64.decode(self.base64string)
+
 
 class ScanRemoteImage:
     def __init__(self, url):
