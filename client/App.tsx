9ad6fc1c7163c9a3793104195fdc48cb6fa05ef7
jonathan dannel <usixjad@gmail.com>
2020-02-22 15:40:04 -0500

    remove nativebase, add ui kitten

--- client/App.tsx
+++ client/App.tsx
@@ -5,6 +5,9 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import { rootReducer } from "./reducer";
 
+import { ApplicationProvider, Layout, Text } from "@ui-kitten/components";
+import { mapping, light as lightTheme } from "@eva-design/eva";
+
 import HomeView from "./Views/HomeView";
 import CameraView from "./Views/CameraView";
 import PhotoView from "./Views/PhotoView";
@@ -15,13 +18,15 @@ const Stack = createStackNavigator();
 function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeView} />
-          <Stack.Screen name="Camera" component={CameraView} />
-          <Stack.Screen name="Photo" component={PhotoView} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <ApplicationProvider mapping={mapping} theme={lightTheme}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Home" component={HomeView} />
+            <Stack.Screen name="Camera" component={CameraView} />
+            <Stack.Screen name="Photo" component={PhotoView} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ApplicationProvider>
     </Provider>
   );
 }
